80c89ff9906c532d08eec6ed9af6e3a00d9f432b
Subhash Barela <barelasubhash16@gmail.com>
2023-02-17 05:28:44 -0800

    Find distance Btw two line

---
+++ FindDistancBtwLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComputationUsingOpps
+{
+    internal class FindDistancBtwLine
+    {
+    
+        
+            public  void length(double x1, double y1, double x2, double y2)
+            {
+                double Distance = Math.Sqrt((x2 - x1) * 2 + (y2 - y1) * 2);
+                Console.WriteLine("Length Between two Line is : " + Distance);
+            }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComputationUsingOpps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindDistancBtwLine Obj= new FindDistancBtwLine();
+            Obj.length(10,20,30 ,20);
+        }
+    }
+}
